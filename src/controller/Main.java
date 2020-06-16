d338ebe28e39314e80f89094c25872b3f2f830b7
krishnamohan <krishnamohan.py@gmail.com>
2020-06-16 12:02:01 +0530

    added

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -8,6 +8,17 @@ public class Main {
 	public static void main(String args[]) {
 		
 	// Fill your code
+		ConnectionManager connect= new ConnectionManager();
+		Connection con=null;
+		con=connect.getConnection();
+		if(con!=null)
+		{
+			System.out.println("Connection established");
+		}
+		else
+		{
+			System.out.println("Check your connection");
+		}
 		
 	}
 }
